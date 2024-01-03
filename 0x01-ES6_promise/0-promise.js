c6a184d703f05b6f85a293f991d5141ccf03e776
xaldovah <dxaldovah@gmail.com>
2024-01-03 12:18:05 +0000

    Return a Promise using this prototype function getResponseFromAPI()

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,8 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const mockApiResponse = {
-      status: 200,
-      body: 'mock-response-data',
-    };
-
-    setTimeout(() => {
-      resolve(mockApiResponse);
-    }, 1000);
-  });
+  const myPromise = new Promise(
+    (resolve) => {
+      resolve();
+    },
+  );
+  return myPromise;
 }
--- 0x01-ES6_promise/utils.js
+++ 0x01-ES6_promise/utils.js
@@ -1,10 +1,14 @@
-const utils = {
-  uploadPhoto: () => {
-    console.log('Uploading photo...');
-  },
-  createUser: () => {
-    console.log('Creating user...');
-  },
-};
-
-module.exports = utils;
+export function uploadPhoto() {
+    return Promise.resolve({
+      status: 200,
+      body: 'photo-profile-1',
+    });
+  }
+  
+  
+  export function createUser() {
+    return Promise.resolve({
+      firstName: 'Guillaume',
+      lastName: 'Salva',
+    });
+  }

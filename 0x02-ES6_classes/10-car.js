710a2a7650a727cddcbef4d77a7c937b9e786e3e
xaldovah <dxaldovah@gmail.com>
2024-01-05 17:10:57 +0000

    Implement a class named Car

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,28 @@
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
+  }
+
+  get brand() {
+    return this[_brand];
+  }
+
+  get motor() {
+    return this[_motor];
+  }
+
+  get color() {
+    return this[_color];
+  }
+
+  cloneCar() {
+    // Create a new instance of the class with the same properties
+    return new this.constructor(this[_brand], this[_motor], this[_color]);
+  }
+}

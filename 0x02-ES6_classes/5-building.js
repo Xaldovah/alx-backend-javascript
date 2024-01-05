c0a7625b822b6661198bb5a38bce7b60484751ae
xaldovah <dxaldovah@gmail.com>
2024-01-05 16:41:06 +0000

    Inheritance

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,13 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = this.validateNumber(sqft, 'Sqft');
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
   }
 
   // Getter for sqft
@@ -8,16 +15,8 @@ export default class Building {
     return this._sqft;
   }
 
-  // Abstract method for evacuationWarningMessage
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-
-  // Helper function to validate number type
-  validateNumber(value, attribute) {
-    if (typeof value !== 'number' || isNaN(value)) {
-      throw new TypeError(`${attribute} must be a number`);
-    }
-    return value;
+  // Setter for sqft
+  set sqft(value) {
+    this._sqft = value;
   }
 }

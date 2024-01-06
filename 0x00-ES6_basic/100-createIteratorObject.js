9b918c812c153b4d623803781f9e218ede4af115
xaldovah <dxaldovah@gmail.com>
2024-01-06 07:17:24 +0000

    changed the logic to 100-createIteratorObject

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,40 +1,9 @@
 export default function createIteratorObject(report) {
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
-  const departments = Object.keys(report.allEmployees);
-
-  const iterator = {
-    next() {
-      if (currentDepartmentIndex < departments.length) {
-        const currentDepartment = departments[currentDepartmentIndex];
-        const currentEmployees = report.allEmployees[currentDepartment];
-
-        if (currentEmployeeIndex < currentEmployees.length) {
-          const result = {
-            value: currentEmployees[currentEmployeeIndex],
-            done: false,
-          };
-
-          currentEmployeeIndex++;
-
-          return result;
-        } else {
-          // Move to the next department
-          currentDepartmentIndex++;
-          currentEmployeeIndex = 0;
-          return this.next();
-        }
-      } else {
-        // All departments have been iterated
-        return { value: undefined, done: true };
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
-    },
-  };
-
-  // Make the object iterable
-  iterator[Symbol.iterator] = function () {
-    return this;
-  };
-
-  return iterator;
+    }
+  }());
 }

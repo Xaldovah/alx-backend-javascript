5f171a314198e59017eac1011d0807c70b33d120
xaldovah <dxaldovah@gmail.com>
2024-01-18 00:30:18 +0300

    Brand convention & Nominal typing

---
+++ 0x04-TypeScript/task_5/dist/js/main.js
@@ -0,0 +1,7 @@
+export function sumMajorCredits(subject1, subject2) {
+    return { credits: subject1.credits + subject2.credits };
+}
+export function sumMinorCredits(subject1, subject2) {
+    return { credits: subject1.credits + subject2.credits };
+}
+//# sourceMappingURL=main.js.map

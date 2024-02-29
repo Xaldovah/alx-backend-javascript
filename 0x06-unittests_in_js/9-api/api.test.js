aa123bfb1e86d271c27840365d68b0a89fd8679a
xaldovah <dxaldovah@gmail.com>
2024-02-29 13:22:08 +0300

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const expect = require('chai').expect;
+
+describe('Cart page', () => {
+  it('should return payment methods for cart when :id is a number', (done) => {
+    request.get('http://localhost:7865/cart/123', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 123');
+      done();
+    });
+  });
+
+  it('should return 404 status code when :id is NOT a number', (done) => {
+    request.get('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});

6fb8eb76dc1ac59085bf80c3eafc350884239dea
xaldovah <dxaldovah@gmail.com>
2024-02-29 14:32:17 +0300

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,30 +1,30 @@
 const fs = require('fs');
 
 function countStudents(path) {
-    try {
-        const data = fs.readFileSync(path, 'utf8');
+  try {
+    const data = fs.readFileSync(path, 'utf8');
 
-        const lines = data.trim().split('\n').filter(line => line.trim() !== '');
+    const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
 
-        const counts = {};
+    const counts = {};
 
-        lines.forEach(line => {
-            const [firstName, lastName, age, field, location] = line.split(',');
-            counts[field] = counts[field] ? counts[field] + 1 : 1;
-        });
+    lines.forEach((line) => {
+      const [firstName, lastName, age, field, location] = line.split(',');
+      counts[field] = counts[field] ? counts[field] + 1 : 1;
+    });
 
-        console.log(`Number of students: ${lines.length}`);
+    console.log(`Number of students: ${lines.length}`);
 
-        for (const field in counts) {
-            const firstNameList = lines
-                .filter(line => line.split(',')[3] === field)
-                .map(line => line.split(',')[0])
-                .join(', ');
-            console.log(`Number of students in ${field}: ${counts[field]}. List: ${firstNameList}`);
-        }
-    } catch (error) {
-        throw new Error('Cannot load the database');
+    for (const field in counts) {
+      const firstNameList = lines
+        .filter((line) => line.split(',')[3] === field)
+        .map((line) => line.split(',')[0])
+        .join(', ');
+      console.log(`Number of students in ${field}: ${counts[field]}. List: ${firstNameList}`);
     }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,34 +1,35 @@
 const fs = require('fs');
 
 function countStudents(path) {
-	return new Promise((resolve, reject) => {
-		fs.readFile(path, 'utf8', (error, data) => {
-			if (err) {
-				reject(new Error('Cannot load the database'));
-				return;
-			}
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(new Error('Cannot load the database'));
+                return;
+            }
 
-			const lines = data.trim().split('\n');
-			const students = {};
-			let totalStudents = 0;
+            const lines = data.trim().split('\n').filter(line => line.trim() !== '');
 
-			lines.forEach(line => {
-				const [firstname, lastname, age, field] = line.split(', ');
-				if (firstname && lastname && age && field) {
-					totalStudents++;
-					if (!students[field]) students[field] = [];
-					students[field].push(firstname);
-				}
-			});
+            const counts = {};
 
-			console.log(`Number of students: ${totalStudents}`);
-			Object.keys(students).forEach(field => {
-				console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
-			});
+            lines.forEach(line => {
+                const [firstName, lastName, age, field, location] = line.split(',');
+                counts[field] = counts[field] ? counts[field] + 1 : 1;
+            });
 
-			resolve();
-		});
-	});
+            console.log(`Number of students: ${lines.length}`);
+
+            for (const field in counts) {
+                const firstNameList = lines
+                    .filter(line => line.split(',')[3] === field)
+                    .map(line => line.split(',')[0])
+                    .join(', ');
+                console.log(`Number of students in ${field}: ${counts[field]}. List: ${firstNameList}`);
+            }
+
+            resolve();
+        });
+    });
 }
 
 module.exports = countStudents;

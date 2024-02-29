3abdaedf21f8a9eaf94e7fa51ea16c7ece62cb42
xaldovah <dxaldovah@gmail.com>
2024-02-29 14:06:14 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,8 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\r`);
+  console.log('\n');
   process.stdout.write('This important software is now closing\n');
   process.exit();
 });

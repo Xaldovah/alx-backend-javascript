ee36423829b97b3603e17d154132f1161e1e7438
xaldovah <dxaldovah@gmail.com>
2024-03-16 15:22:11 +0300

    revision of nodejs basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-	const reading = process.stdin.read();
-	if (reading) {
-		process.stdout.write(`Your name is: ${reading}`);
-	}
+  const reading = process.stdin.read();
+  if (reading) {
+    process.stdout.write(`Your name is: ${reading}`);
+  }
 });
 
 process.stdin.on('end', () => {
-	process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
